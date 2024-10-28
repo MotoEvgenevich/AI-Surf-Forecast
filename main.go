7ed41c9fd184a86999c2a1f2faca2b9985137e46
Artem Kortelev <artemevgenevich89@gmail.com>
2024-10-28 13:00:10 +0400

    create add time period for forecast

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"ai-forecast/structs"
 	"fmt"
 	"time"
 )
 
+type WeatherConditions struct {
+	Sea  structs.MarineForecast
+	Wind structs.WeatherForecast
+}
+
+var DataWeather []WeatherConditions
+
 func main() {
 	currentTime := time.Now()
 	currentDate := currentTime.Format("2006-01-02T")
-	fmt.Println(currentDate)
 	currentDateSlice := make([]string, 0)
 	h := 0
 	hour := "00-00"
@@ -26,8 +33,34 @@ func main() {
 		hour = fmt.Sprintf("%02d-00", h)
 	}
 
+	// Печать всех дат и времени
 	for _, v := range currentDateSlice {
 		fmt.Println(v)
 	}
-	fmt.Println(len(currentDateSlice))
+
+	// Добавление данных о погоде в слайс структур
+	for _, v := range currentDateSlice {
+		// Заполняем поле времени в MarineForecast и WeatherForecast
+		sea := structs.MarineForecast{
+			Current: structs.CurrentDataMarine{
+				Time: v, // Добавляем дату и время в поле Time
+			},
+		}
+		wind := structs.WeatherForecast{
+			Current: structs.CurrentWind{
+				Time: v, // Добавляем дату и время в поле Time
+			},
+		}
+
+		// Добавляем структуру в слайс DataWeather
+		DataWeather = append(DataWeather, WeatherConditions{
+			Sea:  sea,
+			Wind: wind,
+		})
+	}
+
+	// Печать содержимого DataWeather для проверки
+	for _, data := range DataWeather {
+		fmt.Printf("Sea Time: %s, Wind Time: %s\n", data.Sea.Current.Time, data.Wind.Current.Time)
+	}
 }
